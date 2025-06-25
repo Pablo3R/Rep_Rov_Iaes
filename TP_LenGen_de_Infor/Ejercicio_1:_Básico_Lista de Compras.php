8e6b556a7454009df2f36c81f441d73e2cee15dc
Pablo3R <pablo3rovira@gmail.com>
2025-06-25 18:23:02 -0300

    v2

--- TP_LenGen_de_Infor/Ejercicio_1:_Básico_Lista de Compras.php
+++ TP_LenGen_de_Infor/Ejercicio_1:_Básico_Lista de Compras.php
@@ -1,11 +1,39 @@
 <?php
 $lista_compras = ["Pan", "Harina", "Huevos", "Azúcar", "Limón"];
 
-function printVector(Array $vector){
-    foreach($vector as $value){
-        echo "$value";
-    }
+function printVector_Viñeta(Array $vector){
+    foreach($vector as $value){ echo "<li>$value</li>";}
 }
 
-echo "<h1>Lista de compras:</h1><p><?php printVector($lista_compras); ?></p>";
+function existeInVector(Array $vector, string $busqueda){
+    $retorno = 0;
+    foreach($vector as $value){
+        if($value == $busqueda){
+            $retorno = 1;
+        }
+    }
+    return $retorno;
+}
+
+//Comentario
+echo "<h1>Lista de compras:</h1><p>";
+printVector_Viñeta($lista_compras);
+echo "</p>";
+
+$lista_compras[] = "Leche";
+$lista_compras[] = "Carbón";
+
+echo "<p>Actualmente hay "; 
+echo count($lista_compras);
+echo " elementos en la lista.</p>";
+
+$buscando = "Leche";
+
+if(existeInVector($lista_compras, $buscando)){
+    echo "Existe el elemento: <strong><li>$buscando</li></strong> en la lista.";
+}
+else{
+    echo "No existe el elemento: <strong><li>$buscando</li></strong> en la lista.";
+}
+
 ?>
